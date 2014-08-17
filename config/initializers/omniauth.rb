34a87bcbfd4cf94681b3c5bbe40f7a174746736d
Seth <seth.yanow@gmail.com>
2014-08-17 08:20:04 +0000

    make omniauth log to rails logger

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,3 +3,4 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'mbdSIYkKsfPccwMtKijEHBZcp', 'bMpITuWo3ZGn3csMHXM4rwMhDZ93CMfqeibnvkfYIOEoU0gyNS'
   provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', display: 'popup'
 end
+OmniAuth.config.logger = Rails.logger
