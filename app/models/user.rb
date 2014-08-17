fded58b3bf48e29b9dd04f543a90a7dd839e0b3d
Seth <seth.yanow@gmail.com>
2014-08-17 08:18:06 +0000

    lets play why isn't heroku working

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,9 @@ class User < ActiveRecord::Base
     create! do |user|
       user.provider = auth["provider"]
       user.uid = auth["uid"]
-      user.nickname = auth.info["nickname"]
-      user.name = auth.info["name"]
-      user.image = auth.info["image"]
+      user.nickname = auth.info.nickname
+      user.name = auth.info.name
+      user.image = auth.info.image
       user.description = auth["description"]
     end
   end
