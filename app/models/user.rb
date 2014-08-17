ff89a07b10fa548af0571dd5cb419af9c7388ada
Seth Yanow <seth.yanow@gmail.com>
2014-08-17 18:01:49 -0400

    fixed up omniauth, TODO change twitter keys

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ActiveRecord::Base
   def self.from_omniauth(auth)
     where(auth.slice("provider", "uid")).first || create_from_omniauth(auth)
   end
-    
+
   def self.create_from_omniauth(auth)
     create! do |user|
       user.provider = auth["provider"]
@@ -13,26 +13,6 @@ class User < ActiveRecord::Base
       user.name = auth.info.name
       user.image = auth.info.image
       user.description = auth["description"]
-    end
-  end
-  
-  
-  
-  def self.find_or_create_from_auth_hash (auth)
-
-    #find_by_auth_hash(hash)
-    self.create_with_omniauth(auth)
-  end
-
-  def self.find_by_auth_hash (auth)
-
-  end
-
-  def self.create_with_omniauth (auth)
-    create! do |user|
-      user.provider = auth["provider"]
-      user.email = auth["email"]
-      user.uid = auth["uid"]
     end
   end
 end
--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :developer unless Rails.env.production?
-  provider :twitter, 'mbdSIYkKsfPccwMtKijEHBZcp', 'bMpITuWo3ZGn3csMHXM4rwMhDZ93CMfqeibnvkfYIOEoU0gyNS'
-  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', display: 'popup'
+  provider :twitter, ENV['TWITTER_KEY'], ENV['TWITTER_SECRET']
+  provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], scope: 'email', display: 'page'
 end
