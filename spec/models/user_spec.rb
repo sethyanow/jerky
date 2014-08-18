5a67f4a3c4e5ce7bb666ed2d98d3a003f275a7b1
Seth <seth.yanow@gmail.com>
2014-08-18 03:21:35 +0000

    I should spell uid right

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
 
   it { should respond_to(:email) }
   it { should validate_presence_of(:name)}
-  it { should validate_presence_of(:ud)}
+  it { should validate_presence_of(:uid)}
   it { should validate_presence_of(:provider)}
   
   it "#email returns a string" do
