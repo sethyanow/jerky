ec116643e1e531c100fea66047986713a9620a1d
Seth <seth.yanow@gmail.com>
2014-08-18 02:59:36 +0000

    adjust user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,10 +5,10 @@ describe User do
   subject { @user }
 
   it { should respond_to(:email) }
-  it { should validate_presence_of(:email)}
   it { should validate_presence_of(:name)}
-
-
+  it { should validate_presence_of(:ud)}
+  it { should validate_presence_of(:provider)}
+  
   it "#email returns a string" do
     expect(@user.email).to match 'user@example.com'
   end
