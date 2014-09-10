11a82f4a34f7ea2a85b73824e140c17bc819567f
Seth Yanow <seth.yanow@gmail.com>
2014-09-10 14:27:13 -0400

    last little refactor to storecontroller

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -12,31 +12,16 @@ class StoreController < ApplicationController
   end
 
   def build_cart
-    @cart ||= current_user.cart || Cart.create
+    @cart = current_user.cart ||= Cart.create
     session[:cart_id] =  @cart.id
   end
 
-  def assign_cart
-    # => Assign the cart to the user, vice versa and save both
-    @cart.user_id = @user.id
-    @cart.save
-    @user.cart_id = @cart.id
-    @user.save
-  end
-
-  # fn:Setup
-  #   Quick (as possible) handler for requests to the store controller
-  # => Short-circuit instance variables
-  # => Confirm user's logged in
-  # =>
   def setup
     @item ||= Item.new
 
     if current_user
-      @user ||= User.find(current_user.id)
-
+      @user = current_user
       build_cart
-      assign_cart
     else
       display_signin
     end
