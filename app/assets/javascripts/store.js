c1f067f40731ab49555ac3c92f78c89330f80ecc
Seth Yanow <seth.yanow@gmail.com>
2014-09-11 00:50:29 -0400

    clean up form a bit

--- app/assets/javascripts/store.js
+++ app/assets/javascripts/store.js
@@ -7,4 +7,9 @@ function store_setup() {
   $("#item_flavor_id").val(11);
   $("#item_type_id").val(1);
   $("#item_size_id").val(1);
+
+  $("#checkout_button").click(function() {
+    window.location.replace("/checkout");
+    return false; // prevent default behavior
+  });
 }
--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,6 +1,4 @@
 class Cart < Order
   belongs_to :user
   has_many :items
-
-
 end
